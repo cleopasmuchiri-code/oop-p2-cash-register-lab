fd8ad83cd5bc5acc51ac1b48c041e110853bd398
CleopasMMuchiri <cleopasmmuchiri@gmail.com>
2026-09-08 14:26:20 +0300

    fix done

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -13,21 +13,26 @@ class CashRegister:
 
   @discount.setter
   def discount(self, discount):
-    if not isinstance(discount, int) or 0 > discount > 100:
+    if not isinstance(discount, int) or discount < 0 or discount > 100:
       raise ValueError("Not valid discount")
     else:
       self._discount = discount
 
-  def add_item(self, price, quantity):
+  def add_item(self, title, price, quantity=1):
     self.total += (price * quantity)
-    self.items.extend([price] * quantity)
+    self.items.extend([title] * quantity)
     self.previous_transactions.append(price * quantity)
 
   def apply_discount(self):
-    if self.discount > 0 or self.items != []:
+    if self.discount > 0 and self.items:
       discount_amount = (self.discount / 100) * self.total
       self.total -= discount_amount
       self.previous_transactions[-1] = self.total
-
+      print(f"After the discount, the total comes to ${self.total:g}.")
     else:
       print("There is no discount to apply.")
+
+  def void_last_transaction(self):
+    if self.previous_transactions:
+      last = self.previous_transactions.pop()
+      self.total -= last
